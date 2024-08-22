e9b78e935f910a4b105d724bb788784bff2b9c41
Герасимов Дмитрий Геннадьевич <gerasimov@action-media.ru>
2024-08-22 08:50:58 +0200

    main

--- frontend/src/app/posts/[slug]/page.tsx
+++ frontend/src/app/posts/[slug]/page.tsx
@@ -1,10 +1,8 @@
 "use client" // TODO Временно, пока есть useState
 import { v4 } from "uuid";
 import Post from "../../../components/Post";
-import { PostItemType } from "../../../constants/post";
-import { useState } from "react";
-import { IPost, IPostIBodyItem, IPostsShowApiConfigQuery } from "../../../types/post";
-import { Grid, Paper } from "@mui/material";
+import { IPost, IPostsShowApiConfigQuery } from "../../../types/post";
+import { CircularProgress, Grid, Paper } from "@mui/material";
 import { postsShowApiConfig } from "../../../api/apiConfigs/posts/show";
 import { useCustomQuery } from "../../../api/hooks/useCustomQuery";
 
@@ -20,50 +18,34 @@ const Page = ({ params }: IPageProps) => {
     postsShowApiConfig(params.slug)
   )
 
-  const [title] = useState<string>('Фейковый заголовок');
-  const [body, setbody] = useState<Array<IPostIBodyItem>>([
-    {
-      type: PostItemType.H2,
-      value: 'Это Mock заголовок 1',
-      id: v4(),
-    },
-    {
-      type: PostItemType.P,
-      value: 'Это Mock параграф 1 lorem',
-      id: v4(),
-    },
-    {
-      type: PostItemType.H2,
-      value: 'Это Mock заголовок 2',
-      id: v4(),
-    },
-    {
-      type: PostItemType.P,
-      value: 'Это Mock параграф 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id purus metus. Aenean euismod magna eget mauris consequat, at volutpat metus sollicitudin.',
-      id: v4(),
-    }
-  ]);
+  if (isLoading && !isSuccess) {
+    return (
+      <CircularProgress />
+    )
+  }
 
-  return (
-    <Grid container spacing={2} sx={{ padding: 2 }}>
-      {/* Main content (Post) */}
-      <Grid item xs={12} md={8}>
-        <Post
-          title={title}
-          items={body}
-        />
-      </Grid>
+  if (isSuccess) {
+    return (
+      <Grid container spacing={2} sx={{ padding: 2 }}>
+        {/* Main content (Post) */}
+        <Grid item xs={12} md={8}>
+          <Post
+            title={data.title}
+            items={[]}
+          />
+        </Grid>
 
-      {/* Sidebar (Aside) */}
-      <Grid item xs={12} md={4}>
-        <Paper sx={{ padding: 2 }}>
-          {/* Здесь будет контент боковой панели */}
-          <h2>Боковая панель</h2>
-          <p>Дополнительная информация или ссылки...</p>
-        </Paper>
+        {/* Sidebar (Aside) */}
+        <Grid item xs={12} md={4}>
+          <Paper sx={{ padding: 2 }}>
+            {/* Здесь будет контент боковой панели */}
+            <h2>Боковая панель</h2>
+            <p>Дополнительная информация или ссылки...</p>
+          </Paper>
+        </Grid>
       </Grid>
-    </Grid>
-  );
+    );
+  }
 }
 
 export default Page;
--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from .views import index, show, update, delete, add
 
 urlpatterns = [
     path('', index),
-    path('<str:slug>/', show),
+    path('<str:slug>', show),
     path('add', add),
     path('update', update),
     path('delete', delete),
